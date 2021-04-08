98864c9958f8ff7c7365a839658374a35061db9d
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-04-08 15:26:16 +0300

    Add visual element factory class

---
+++ Assets/UnityLocalization/Editor/Utility/VisualElementFactory.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEngine.UIElements;
+
+namespace UnityLocalization.Utility {
+    public static class VisualElementFactory {
+        public static T Create<T>(string name = null, params string[] classNames) where T : VisualElement, new() {
+            var t = new T {name = name};
+            foreach (var className in classNames) {
+                t.AddToClassList(className);
+            }
+            return t;
+        }
+
+        public static VisualElement NthChild(this VisualElement element, int n) {
+            return element[n];
+        }
+        public static T NthChild<T>(this VisualElement element, int n) where T : VisualElement {
+            return element.NthChild(n) as T;
+        }
+
+        public static VisualElement NthParent(this VisualElement element, int n) {
+            var parent = element;
+            for (var i = 0; i < n; i++) {
+                parent = parent.parent;
+            }
+            return parent;
+        }
+        public static T NthParent<T>(this VisualElement element, int n) where T : VisualElement {
+            return element.NthParent(n) as T;
+        }
+
+        public static T Add<T, T2>(this T element, string name = null, params string[] classNames) where T : VisualElement where T2 : VisualElement, new() {
+            var t2 = Create<T2>(name, classNames);
+            element.Add(t2);
+            return element;
+        }
+        public static T2 AddGet<T, T2>(this T element, string name = null, params string[] classNames) where T : VisualElement where T2 : VisualElement, new() {
+            var t2 = Create<T2>(name, classNames);
+            element.Add(t2);
+            return t2;
+        }
+
+        public static T Do<T>(this T element, Action action) where T : VisualElement {
+            action?.Invoke();
+            return element;
+        }
+        
+        public static T Do<T>(this T element, Action<T> action) where T : VisualElement {
+            action?.Invoke(element);
+            return element;
+        }
+        
+        public static TextField TextFieldWithPlaceholder(string name, string label, string value, string placeholderText, string placeholderName) {
+            return Create<TextField>(name).Do(self => {
+                self.label = label;
+                self.value = value;
+                self.AddToClassList("textFieldWithPlaceholder");
+            }).Q("unity-text-input").AddGet<VisualElement, Label>(placeholderName, "placeholderLabel").Do(self => {
+                self.text = placeholderText;
+                if(!string.IsNullOrEmpty(value)) self.AddToClassList("hidden");
+            }).NthParent<TextField>(2).Do(self => {
+                var placeholderLabel = self.Q<Label>(placeholderName, "placeholderLabel");
+                self.RegisterValueChangedCallback(evt => {
+                    if (string.IsNullOrEmpty(evt.newValue)) placeholderLabel.RemoveFromClassList("hidden");
+                    else placeholderLabel.AddToClassList("hidden");
+                });
+            });
+        }
+    }
+}
